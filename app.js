2686670deb59cf46657d28923b5854e2908ed1b9
GitShaAli <shafeeqalibn@outlook.com>
2022-08-11 15:22:55 +0530

    TASK 8 q.1

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server= http.createServer((req,res)=>{
-    console.log(req.url,req.method,req.headers);
+    // console.log(req.url,req.method,req.headers);
     res.setHeader('Content-Type','text/html');
-    if(req.url=='/home'){
+    const method = req.method;
+    if(req.url=='/'){
         res.write('<html>')
         res.write('<head><title>First Page</title></head>')
-        res.write(`<body><h1>Welcome home</h1></body>`)
+        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
         res.write('</html>')
-        res.end();
+        return res.end();
     }
-    else if(req.url=='/about'){
-        res.write('<html>')
-        res.write('<head><title>First Page</title></head>')
-        res.write(`<body><h1>Welcome to About Us page</h1></body>`)
-        res.write('</html>')
-        res.end();
-    }  
-    else if(req.url=='/node'){
-        res.write('<html>')
-        res.write('<head><title>First Page</title></head>')
-        res.write(`<body><h1>Welcome to my Node Js project</h1></body>`)
-        res.write('</html>')
-        res.end();
+    if(req.url=='/message' && method=="POST"){
+        fs.writeFileSync('message.txt','DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
     }
-    else{
+    
         res.write('<html>')
         res.write('<head><title>First Page</title></head>')
         res.write(`<body><h1>Welcome to main Page</h1></body>`)
         res.write('</html>')
         res.end();
-    }
-    // let str = '';
-    // if(req.url=='/home')    str='Welcome home';
-    // else if(req.url=='/about')    str='Welcome to About Us page';
-    // else if(req.url=='/node')    str='Welcome to my Node Js project';
-    // else                    str='Welcome to main Page';
-    // res.setHeader('Content-Type','text/html');
-    // res.write('<html>')
-    // res.write('<head><title>First Page</title></head>')
-    // res.write(`<body><h1>${str}</h1></body>`)
-    // res.write('</html>')
-    // res.end();
 });
 
-server.listen(4000)
+server.listen(3000)
