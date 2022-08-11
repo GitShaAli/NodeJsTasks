8926427bffb45ff24313e7bd7db39cc94fe2b806
GitShaAli <shafeeqalibn@outlook.com>
2022-08-11 16:24:31 +0530

    Top of Form

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ const server= http.createServer((req,res)=>{
     res.setHeader('Content-Type','text/html');
     const method = req.method;
     if(req.url=='/'){
+
         res.write('<html>')
         res.write('<head><title>First Page</title></head>')
-        res.write(`<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`)
+        const msg = fs.readFileSync('./message.txt', 'utf8');
+        res.write(`<body><form action="/message" method="POST"><p style="color:red;">${msg}</p><input type="text" name="message"><button type="submit">Send</button></form></body>`)
         res.write('</html>')
         return res.end();
     }
     if(req.url=='/message' && method=="POST"){
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        })
+        return req.on('end',()=>{
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            fs.writeFile('message.txt',message,err=>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+        })
     }
     
         res.write('<html>')
