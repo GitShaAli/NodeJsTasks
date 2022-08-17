b574cda56cc4840eaa4281a78fc90c8e4e7fd562
GitShaAli <shafeeqalibn@outlook.com>
2022-08-17 13:16:23 +0530

    TASK2

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const http = require('http');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
 // const server= http.createServer(routes)
 
-console.log(routes.someText)
+// console.log(routes.someText)
 
-const server= http.createServer(routes.handler)
+const express = require('express');
 
-server.listen(3000)
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log("In the middleware")
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("In the next middleware");
+    res.send('<h1> hello to node js </h1>') ;
+    // res.send( { key1: "value" })
+})
+// const server= http.createServer(app)
+
+// server.listen(3000)
+
+app.listen(3000);
