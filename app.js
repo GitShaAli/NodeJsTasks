d7c64fa82bee4f6c93c381186858821e54927282
GitShaAli <shafeeqalibn@outlook.com>
2022-08-17 20:42:19 +0530

    Task 4 ( Routers and Filters )

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoute = require('./routes/admin');
+
+const shopRoute = require(`./routes/shop`);
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.use('/admin',adminRoute);
+app.use('/shop',shopRoute);
+
+
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
+app.listen(3000);
+
+
+// const http = require('http');
 
 // const routes = require('./routes');
-
 // const server= http.createServer(routes)
 
 // console.log(routes.someText)
@@ -11,25 +31,12 @@ const http = require('http');
 //     next();
 // })
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+// app.use('/add-product',(req,res,next)=>{
+//     res.send('<form action="/product" method = "POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>') ;
+// })
 
+// app.post('/product',(req,res,next)=>{
+//     console.log(`Title : ${req.body.title} &  Size : ${req.body.size}`);
+//     res.redirect('/');
+// })
 
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method = "POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>') ;
-})
-
-app.post('/product',(req,res,next)=>{
-    console.log(`Title : ${req.body.title} &  Size : ${req.body.size}`);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1> hello to node js </h1>') ;
-})
-
-
-app.listen(3000);
